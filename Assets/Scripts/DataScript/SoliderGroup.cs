cf27942bd3290c720ea300bba93450d7de21fb09
lox916445710 <3291354250@qq.com>
2023-08-21 23:31:07 +0800

    8.21 制作了第一版的单位重整队形功能

--- Assets/Scripts/DataScript/SoliderGroup.cs
+++ Assets/Scripts/DataScript/SoliderGroup.cs
@@ -10,8 +10,9 @@ public class SoliderGroup : MonoBehaviour
     /// 展示生成位置的虚影
     /// </summary>
     public Transform[] soldiers;
+    public Vector3[] unitOffsetsToCaptain;
     /// <summary>
-    /// 作为子对象的士兵s
+    /// 所有在场内生成且存活的，作为子对象的士兵s
     /// </summary>
     public List<Entity> createdSoldiers;
     /// <summary>
@@ -22,14 +23,43 @@ public class SoliderGroup : MonoBehaviour
     [HideInInspector]
     public string Id;
     public SpriteRenderer flagSprite;
-
-    Entity flagFollowingSoldier;
+    /// <summary>
+    /// 当前旗帜跟随的单位，也就是一队的队长
+    /// </summary>
+    public Entity flagFollowingSoldier;
+    /// <summary>
+    /// 完成重整阵型的队员的数量，用于判断整个队伍的重整阵型是否全部完成
+    /// </summary>
+    int settledUnitAmount;
+    /// <summary>
+    /// 是否正在重整阵型
+    /// </summary>
+    bool isReorganizing;
     private void Start()
     {
+        Initialize();
+    }
+
+    private void Update()
+    {
+        CheckUnitBackingToRelativePos();
+    }
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    public void Initialize()
+    {
+        unitOffsetsToCaptain = new Vector3[soldiers.Length];
+        Debug.Log("soldier数量：" + soldiers.Length);
+
         //给残影对象加上面向相机的效果
         foreach (Transform t in soldiers)
         {
             t.gameObject.AddComponent<FaceToCamera>();
+        }
+        for (int i = 0; i < soldiers.Length; i++)
+        {
+            unitOffsetsToCaptain[i] = soldiers[0].transform.position - soldiers[i].transform.position;
         }
     }
     /// <summary>
@@ -76,6 +106,7 @@ public class SoliderGroup : MonoBehaviour
             createdSoldiers.Add(e);
 
         }
+
         //生成之后摧毁虚影
         foreach (Transform t in soldiers)
         {
@@ -85,6 +116,8 @@ public class SoliderGroup : MonoBehaviour
         //跟随
         //gameObject.AddComponent<FaceToCamera>();
         SetFlagFollowSoldier();
+        //设置具体士兵和队长的相对位置
+        SetUnitOffset();
         //transform.SetParent(createdSoldiers[0].transform);
     }
     //当组内士兵死亡时调用的方法
@@ -113,5 +146,86 @@ public class SoliderGroup : MonoBehaviour
         flagFollowingSoldier = createdSoldiers[0];
         flagSprite.gameObject.transform.localPosition = new Vector3(0, 8 / createdSoldiers[0].transform.localScale.x, flagSprite.transform.localPosition.z);
     }
+    /// <summary>
+    /// 设置各个对象的相对阵型位置
+    /// </summary>
+    public void SetUnitOffset()
+    {
 
+        for (int i = 0; i < createdSoldiers.Count; i++)
+        {
+            Debug.Log(unitOffsetsToCaptain.Length);
+            if (unitOffsetsToCaptain[i] != null)
+                createdSoldiers[i].OffsetToCaptain = unitOffsetsToCaptain[i];
+        }
+    }
+    /// <summary>
+    /// 遍历所有生成了的士兵，让其处于正在回到相对位置的状态之中（原先有给队长上减速buff，但是后面改成了队长在原地等待其他士兵汇合
+    /// </summary>
+    public void SetUnitBackingToRelativePos()
+    {
+        //BuffManager.instance.EntityAddBuff(createdSoldiers[0], "2000001");
+        for (int i = 0; i < createdSoldiers.Count; i++)
+        {
+            createdSoldiers[i].SetBackingToRelativePos(true);
+
+        }
+        settledUnitAmount = 0;
+        isReorganizing = true;
+    }
+    /// <summary>
+    /// 检测士兵的回到相对位置是否完成
+    /// </summary>
+    public void CheckUnitBackingToRelativePos()
+    {
+        if (isReorganizing)
+        {
+            //遍历所有生成了的士兵，如果其位置和阵型中的相对位置非常接近，则视为它完成了自己的重整阵型，这时这个士兵在原地待命，完成重整的士兵数量+1
+            //当完成重整的士兵数量等于存活的士兵总数量时，视为整个队伍完成重整，队伍再一起移动
+            foreach (Entity e in createdSoldiers)
+            {
+                if (Vector3.Distance(e.transform.position, e.parentSoldierGroup.createdSoldiers[0].transform.position - e.OffsetToCaptain) < 0.1f)
+                {
+                    if (e.GetBackingToRelativePos() == true)
+                    {
+
+                        //此时已经回到相对位置，在所有士兵到齐之前原地待命
+                        //if (e != createdSoldiers[0])
+                        //{
+                            e.SetSpeedBuff(-100);
+                        //}
+
+                        //完成重整的士兵数量+1
+                        settledUnitAmount++;
+
+                        //Debug.Log("数量增加！当前：" + settledUnitAmount + "目标：" + createdSoldiers.Count);
+                    }
+                    //这名士兵判定为完成了自己的重整阵型
+                    e.SetBackingToRelativePos(false);
+
+                    //Debug.Log("回到了相对位置" + name);
+
+
+                }
+                //当所有士兵到齐，解除士兵的原地待命
+                if (settledUnitAmount == createdSoldiers.Count)
+                {
+                    foreach (Entity e2 in createdSoldiers)
+                    {
+                        //if (e2 != createdSoldiers[0])
+                        //{
+                            Debug.Log("解除待命："+e2.name);
+                            e2.SetSpeedBuff(100);
+                        //}
+                    }
+                    //BuffManager.instance.EntityRemoveBuff(createdSoldiers[0], "2000001");
+                    //Debug.Log("解除了队长的减速buff");
+
+                    //初始化相关变量
+                    isReorganizing = false;
+                    settledUnitAmount = 0;
+                }
+            }
+        }
+    }
 }
