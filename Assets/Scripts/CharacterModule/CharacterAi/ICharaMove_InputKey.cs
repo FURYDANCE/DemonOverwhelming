06f46dfde53ac5054359b5696a74aafe8828f3e3
lox916445710 <3291354250@qq.com>
2023-09-12 19:59:53 +0800

    9.12 20:00 添加InputSystem并作用到玩家输入

--- Assets/Scripts/CharacterModule/CharacterAi/ICharaMove_InputKey.cs
+++ Assets/Scripts/CharacterModule/CharacterAi/ICharaMove_InputKey.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 public class ICharaMove_InputKey : MonoBehaviour, ICharacterMove
 {
     Entity entity;
@@ -9,7 +11,10 @@ public class ICharaMove_InputKey : MonoBehaviour, ICharacterMove
     public float moveDir_Y;
     public Vector3 moveDir;
     CharacterStateManager manager;
-    
+    bool m_Left;
+    bool m_Right;
+    bool m_Up;
+    bool m_Down;
     public void Moving()
     {
         transform.position = Vector3.MoveTowards(transform.position, moveDir, speed * Time.deltaTime);
@@ -28,15 +33,15 @@ public class ICharaMove_InputKey : MonoBehaviour, ICharacterMove
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (m_Left)
             moveDir_X = -1;
-        else if (Input.GetKey(KeyCode.D))
+        else if (m_Right)
             moveDir_X = 1;
         else
             moveDir_X = 0;
-        if (Input.GetKey(KeyCode.S))
+        if (m_Down)
             moveDir_Y = -1;
-        else if (Input.GetKey(KeyCode.W))
+        else if (m_Up)
             moveDir_Y = 1;
         else
             moveDir_Y = 0;
@@ -44,5 +49,60 @@ public class ICharaMove_InputKey : MonoBehaviour, ICharacterMove
         //当有输入的时候不能进入追击状态，仅当无输入的时候可以进入
         if (moveDir_X == 0 && moveDir_Y == 0)
             manager.SetAttackTarget(manager.enemyCheckScript.EnemyCheck());
+    }
+    public void OnMoveRight(InputAction.CallbackContext value)
+    {
+
+        if (value.phase == InputActionPhase.Performed)
+            m_Right = true;
+        //moveDir_X = 1;
+
+
+        if (value.phase == InputActionPhase.Canceled)
+            m_Right = false;
+
+
+        //moveDir = transform.position + new Vector3(moveDir_X, moveDir_Y, 0).normalized;
+
+    }
+    public void OnMoveLeft(InputAction.CallbackContext value)
+    {
+        if (value.phase == InputActionPhase.Performed)
+            m_Left = true;
+        if (value.phase == InputActionPhase.Canceled)
+            m_Left = false;
+
+        //moveDir = transform.position + new Vector3(moveDir_X, moveDir_Y, 0).normalized;
+
+
+    }
+    public void OnMoveUp(InputAction.CallbackContext value)
+    {
+        Debug.Log("111");
+        if (value.phase == InputActionPhase.Performed)
+            m_Up = true;
+        if (value.phase == InputActionPhase.Canceled)
+            m_Up = false;
+
+        //moveDir = transform.position + new Vector3(moveDir_X, moveDir_Y, 0).normalized;
+
+
+    }
+    public void OnMoveDown(InputAction.CallbackContext value)
+    {
+        if (value.phase == InputActionPhase.Performed)
+            m_Down = true;
+        if (value.phase == InputActionPhase.Canceled)
+            m_Down = false;
+        //moveDir_Y = 0;
+        //moveDir = transform.position + new Vector3(moveDir_X, moveDir_Y, 0).normalized;
+
+
+    }
+    public void OnSkill(InputAction.CallbackContext value)
+    {
+        if (value.phase == InputActionPhase.Performed)
+            entity.UseSkill(1);
+
     }
 }
