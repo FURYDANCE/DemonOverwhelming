195ed26abcbaf8d43f23160a4acb270ce6e376ce
lox916445710 <3291354250@qq.com>
2023-11-20 19:14:38 +0800

    11.20 继续处理RtsEngine相关的问题，加了UI和游戏结束判断

---
+++ Assets/Scripts/RtsEngineScripts/CustomUnit.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+using RTSEngine.Game;
+using RTSEngine.Determinism;
+using RTSEngine;
+using RTSEngine.Entities;
+using RTSEngine.UnitExtension;
+using RTSEngine.EntityComponent;
+using RTSEngine.ResourceExtension;
+using RTSEngine.Event;
+
+namespace DemonOverwhelming
+{
+    public class CustomUnit : EntityComponentBase
+    {
+        protected override void OnInit()
+        {
+
+        }
+    }
+}
--- Assets/Scripts/RtsEngineScripts/UnitWaveSpawner.cs
+++ Assets/Scripts/RtsEngineScripts/UnitWaveSpawner.cs
@@ -5,58 +5,62 @@ using RTSEngine.Determinism;
 using RTSEngine;
 using RTSEngine.Entities;
 using RTSEngine.UnitExtension;
-
-/// <summary>
-/// 用RTSEngine生成单位
-/// </summary>
-public class UnitWaveSpawner : MonoBehaviour, IPostRunGameService
+namespace DemonOverwhelming
 {
-    [SerializeField, EnforceType(typeof(IUnit), prefabOnly: true)]
-    private GameObject unitPrefabObj = null;
-    private IUnit unitPrefab;
-    [SerializeField]
-    private int spawnAmount = 3;
 
-    [SerializeField]
-    private Transform spawnTransform = null;
-    [SerializeField]
-    private Transform gotoTransform = null;
 
-    protected IUnitManager unitMgr { private set; get; }
-
-    [SerializeField]
-    private float spawnPeriod = 5.0f;
-    private TimeModifiedTimer timer;
-
-    public void Init(IGameManager gameMgr)
+    /// <summary>
+    /// 用RTSEngine生成单位
+    /// </summary>
+    public class UnitWaveSpawner : MonoBehaviour, IPostRunGameService
     {
-        timer = new TimeModifiedTimer(spawnPeriod);
-        if (!spawnTransform.IsValid() || !gotoTransform.IsValid() || !unitPrefabObj.IsValid())
+        [SerializeField, EnforceType(typeof(IUnit), prefabOnly: true)]
+        private GameObject unitPrefabObj = null;
+        private IUnit unitPrefab;
+        [SerializeField]
+        private int spawnAmount = 3;
+
+        [SerializeField]
+        private Transform spawnTransform = null;
+        [SerializeField]
+        private Transform gotoTransform = null;
+
+        protected IUnitManager unitMgr { private set; get; }
+
+        [SerializeField]
+        private float spawnPeriod = 5.0f;
+        private TimeModifiedTimer timer;
+
+        public void Init(IGameManager gameMgr)
         {
-            Debug.LogError("[UnitWaveSpawner] All inspector fields must be assigned!");
-            return;
+            timer = new TimeModifiedTimer(spawnPeriod);
+            if (!spawnTransform.IsValid() || !gotoTransform.IsValid() || !unitPrefabObj.IsValid())
+            {
+                Debug.LogError("[UnitWaveSpawner] All inspector fields must be assigned!");
+                return;
+            }
+
+            unitPrefab = unitPrefabObj.GetComponent<IUnit>();
+            unitMgr = gameMgr.GetService<IUnitManager>();
         }
 
-        unitPrefab = unitPrefabObj.GetComponent<IUnit>();
-        unitMgr = gameMgr.GetService<IUnitManager>();
-    }
-
-    private void Update()
-    {
-        if (timer.ModifiedDecrease())
+        private void Update()
         {
-            timer.Reload();
-
-            // Spawn Unit Here
-            for (int i = 0; i < spawnAmount; i++)
+            if (timer.ModifiedDecrease())
             {
-                unitMgr.CreateUnit(unitPrefab, spawnTransform.position, Quaternion.identity, new InitUnitParameters
-                {
-                    free = true,
+                timer.Reload();
 
-                    useGotoPosition = true,
-                    gotoPosition = gotoTransform.position,
-                });
+                // Spawn Unit Here
+                for (int i = 0; i < spawnAmount; i++)
+                {
+                    unitMgr.CreateUnit(unitPrefab, spawnTransform.position, Quaternion.identity, new InitUnitParameters
+                    {
+                        free = true,
+
+                        useGotoPosition = true,
+                        gotoPosition = gotoTransform.position,
+                    });
+                }
             }
         }
     }
